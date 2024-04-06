ddef88a0ebc82bc99c08c3692ed7a06c63d7c85b
Asya <asyafrontend@gmail.com>
2024-04-06 23:47:36 +0300

    changed patterns

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,7 @@ module.exports = {
       },
       backgroundImage: {
         'hand-pattern': "url('../img/hand.svg')",
+        'gift-pattern': "url('../img/icon-gift.svg')"
       }
     },
     plugins: [],
