fb1fe3953e79d1b6e1808c56f9c838f7d41cb511
Asya <asyafrontend@gmail.com>
2024-05-01 23:16:46 +0300

    returned all of them to init values

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['index.html', 'src/main.css'],
+  content: ['index.html'],
   theme: {
     container: {
       center: 'true',
